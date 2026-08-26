1062fa8fb1a15fbebdea0bf25ecc7e81ba96b0bc
dguemo <dguerram@eafit.edu.co>
2026-08-26 11:19:03 -0500

    visualizacion de peliculas con estilo

---
+++ Taller_2/moviereviews/urls.py
@@ -0,0 +1,20 @@
+"""
+URL configuration for moviereviews project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path
+from movie import views as movieViews
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', movieViews.home, name='home'),
+    path('about/', movieViews.about, name='about'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
